9f74e2326b8ef3ebca1f09a94473d636228beded
kevin Silva <kevin.silva>
2021-04-08 14:32:29 -0500

    Fix

--- var/www/html/wp-content/themes/lafka/woocommerce/loop/add-to-cart.php
+++ var/www/html/wp-content/themes/lafka/woocommerce/loop/add-to-cart.php
@@ -41,7 +41,7 @@ if ( lafka_get_option( 'use_quickview' ) && $product->get_type() != 'composite' 
 		$lafka_quickview_link_label = __( 'Mas Opciones', 'lafka' );
 		$classes[] = 'lafka-more-options';
 	} else {
-		$lafka_quickview_link_label = __( 'Order Now', 'lafka' );
+		$lafka_quickview_link_label = __( 'Ordenalo Ahora', 'lafka' );
 	}
 
 	echo '<a href="#" class="'.esc_attr(implode(' ', $classes)).'" data-id="' . esc_attr( $product->get_id() ) . '" title="' . esc_attr( $lafka_quickview_link_label ) . '">' . esc_html( $lafka_quickview_link_label ) . '</a>';
