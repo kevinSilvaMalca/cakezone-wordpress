4ab3d6d81282b67d9fbb8f02c0ae1a35c1552b82
kevin Silva <kevin.silva>
2021-04-09 12:40:11 -0500

    asd

--- var/www/html/wp-content/themes/lafka/woocommerce/loop/add-to-cart.php
+++ var/www/html/wp-content/themes/lafka/woocommerce/loop/add-to-cart.php
@@ -51,8 +51,10 @@ if (lafka_get_option('use_quickview') && $product->get_type() != 'composite') {
 ?>
 		<div class="lafka-variations-in-catalog cart">
 			<span class="lafka-list-variation-price">
-				<span class="price"><?php echo wc_price($product->get_price()); ?></span>
-				<span <?php $lafka_quickview_link_label = __('+', 'lafka'); ?></span>
+				<span class="price">
+					<?php echo wc_price($product->get_price()); ?>
+					<?php $lafka_quickview_link_label = __('+', 'lafka'); ?>
+				</span>
 				<!-- ORDENALO AHORA -->
 			</span>
 		</div>
